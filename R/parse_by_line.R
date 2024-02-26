7684af550706a96d4ba29abc8b9c62df0c64580c
Daniel Thiele <daniel.thiele@fu-berlin.de>
2024-02-26 16:48:31 +0100

    add load users function

--- R/parse_by_line.R
+++ R/parse_by_line.R
@@ -83,3 +83,91 @@ load_tweets_json_line <- function(
 
   return(dt)
 }
+
+
+
+
+#' Load Users from JSON or JSONL File
+#'
+#' This function parses and reshapes .json and .jsonl data containing user information into a format suitable for further analysis.
+#'
+#' @param file_path A character string specifying the path to the .json or .jsonl file.
+#' @param num_threads An integer specifying the number of threads to be used for parsing. Default is NULL.
+#' @param n_max An integer specifying the maximum number of lines to read from the file. Default is Inf.
+#' @param skip_empty_rows A logical value indicating whether empty rows should be skipped. Default is TRUE.
+#' @param query A character string specifying the JSON path to extract user data from. Default is "/includes/users".
+#' @param query_error_ok A logical value indicating whether to continue parsing if a query error occurs. Default is TRUE.
+#' @param on_query_error A function to handle query errors if they occur. Default is NULL.
+#' @param parse_error_ok A logical value indicating whether to continue parsing if a parse error occurs. Default is TRUE.
+#' @param on_parse_error A function to handle parse errors if they occur. Default is NULL.
+#' @param empty_array A value to use for empty arrays encountered during parsing. Default is NULL.
+#' @param empty_object A value to use for empty objects encountered during parsing. Default is NULL.
+#' @param max_simplify_lvl An integer specifying the maximum level of simplification. Default is 2L (vector).
+#' Other options from RcppSimdJson::fparse are data.frame (0L), matrix (1L), vector (2L) or list (3L).
+#'
+#' @return A data.table object containing the parsed and reshaped user data.
+#'
+#' @details This function reads a .json or .jsonl file line by line to avoid potential errors with RcppSimdJson. It then processes the user data into a format suitable for further analysis.
+#'
+#' @examples
+#' \dontrun{
+#'   users <- load_users_json_line("tweets.json")
+#' }
+#'
+#' @references
+#' For more information on the CooRTweet package, see: https://github.com/nicolarighetti/CoRTweet
+#'
+#' @export
+load_users_json_line <- function(
+    file_path,
+    num_threads = NULL,
+    n_max = Inf,
+    skip_empty_rows = TRUE,
+    query = "/includes/users",
+    query_error_ok = TRUE,
+    on_query_error = NULL,
+    parse_error_ok = TRUE,
+    on_parse_error = NULL,
+    empty_array = NULL,
+    empty_object = NULL,
+    max_simplify_lvl = 2L
+) {
+
+  # Pick number of threads if not specified
+  if(is.null(num_threads)){
+    num_threads <- parallel::detectCores()
+  }
+
+  # Parse one json file line-by-line
+  # Note: This is necessary if RcppSimdJson::fload/fparse breaks on the whole file due to corrupt lines
+  # and (I think) for parsing .jsonl files.
+  jsonl <- RcppSimdJson::fparse(readr::read_lines(file = file_path,   # I use readr::read_lines, which has been found to be faster than readLines or vroom::vroom_lines
+                                                  num_threads = num_threads,
+                                                  n_max = n_max,
+                                                  skip_empty_rows = skip_empty_rows),
+                                query = query,
+                                query_error_ok = query_error_ok,
+                                on_query_error = on_query_error,
+                                parse_error_ok = parse_error_ok,
+                                on_parse_error = on_parse_error,
+                                empty_array = empty_array,
+                                empty_object = empty_object,
+                                max_simplify_lvl = max_simplify_lvl) |>
+    purrr::flatten() ## Remove upper layer of list
+
+  # For data.table::rbindlist, 'simple' lists must be restructured into more nested lists..
+  # TO DO: Find a way to skip this step
+  jsonl <- purrr::map(jsonl, ~ purrr::modify(.x, ~ if (is.list(.x) && length(.x) > 1) list(.x) else .x))
+
+  dt <- data.table::rbindlist(jsonl,
+                              use.names = TRUE,
+                              fill = TRUE)
+
+  # rename "id" column
+  data.table::setnames(dt, "id", "user_id")
+
+  # deduplicate
+  dt <- unique(dt, by = "user_id")
+
+  return(dt)
+}
