eabe633ecfe0c2367cd11a12fd189bdf39ffbab2
Daniel Thiele <daniel.thiele@fu-berlin.de>
2024-04-07 17:02:41 +0200

    allow load_preprocess_tweets_jsonl_multi to handle file_path vector, make binding optional

--- R/parse_by_line.R
+++ R/parse_by_line.R
@@ -230,22 +230,54 @@ load_tweets_jsonl_multi <- function(data_dir, ...) {
 
 
 
-#' Load and Preprocess Tweets from Multiple JSON Files
+
+
+#' This function loads tweets from JSONL files and preprocesses them.
 #'
-#' Load and preprocess tweets from multiple JSON files located in the specified directory.
+#' @param data_dir A character string specifying the directory containing JSONL files. Defaults to \code{NULL}.
+#' @param file_paths A character vector specifying the paths of JSONL files. Defaults to \code{NULL}.
+#' @param save_dir A character string specifying the directory to save preprocessed data. Defaults to \code{NULL}.
+#' @param bind Logical. Should results be bound to one list of data tables or returned as list of lists? Default is TRUE.
+#' @param ... Additional arguments to be passed to \code{\link[jsonlite]{fromJSON}} when loading JSONL files.
 #'
-#' @param data_dir Directory containing the JSON files.
-#' @param save_dir Optional. Directory to save the pre-pocessed .rds files - one for each parsed .json.
-#' @param ... Additional arguments to pass to \code{\link{load_tweets_jsonl}} function.
-#' @return A list of data.tables containing preprocessed tweets from all JSON files.
+#' @return A list containing preprocessed tweet data.
 #' @export
-load_preprocess_tweets_jsonl_multi <- function(data_dir,
+#'
+load_preprocess_tweets_jsonl_multi <- function(data_dir = NULL,
+                                               file_paths = NULL,
                                                save_dir = NULL,
+                                               bind = TRUE,
                                                ...) {
-  if (!endsWith(data_dir, "/")) {
-    data_dir <- paste0(data_dir, "/")
+
+  if(!is.null(data_dir)){
+
+    if (!endsWith(data_dir, "/")) {
+      data_dir <- paste0(data_dir, "/")
+    }
+
+    if(!dir.exists(data_dir)){
+      stop(paste0("Please provide a valid path 'data_dir'."))
+    }
+
+    json_files <- Sys.glob(paste0(data_dir, "*.json*"))
+
+  }else{
+
+    if(is.null(file_paths)){
+      stop(paste0("Please provide either 'data_dir' or 'file_paths'."))
+    }
+
+    if(!all(file.exists(file_paths))){
+      warning(paste0("The following files do not exist and will be skipped:",
+                     file_paths[!file.exists(file_paths)]
+      ))
+      json_files <- file_paths[file.exists(file_paths)]
+    }else{
+
+      json_files <- file_paths
+
+    }
   }
-  json_files <- Sys.glob(paste0(data_dir, "*.json*"))
 
   suppressWarnings(
     result_list <-  pbapply::pblapply(json_files, function(file) {
@@ -272,7 +304,12 @@ load_preprocess_tweets_jsonl_multi <- function(data_dir,
     }
     )
   )
-  result <- rbind_preprocessed_dts(result_list)
 
+  if(bind){
+  result <- rbind_preprocessed_dts(result_list)
   return(result)
+  }else{
+  return(result_list)
+  }
+
 }
